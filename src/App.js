23350537de0c601788e759c2c711a6666f7bac2c
Carlos <echeverriacarlos1@gmail.com>
2019-06-23 04:16:32 +0000

    some changes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Landing from './components/Landing'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+      
+        <Landing/>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to Call Echeve
